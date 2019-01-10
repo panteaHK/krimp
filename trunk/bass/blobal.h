1932df9acf885901fa4a34610e2afef2281ccf0d
PanteaHK <pantea.haghighatkhah@gmail.com>
2019-01-10 14:42:31 +0100

    Added type definitions.

--- trunk/bass/blobal.h
+++ trunk/bass/blobal.h
@@ -1,14 +1,14 @@
 //
 // Copyright (c) 2005-2012, Matthijs van Leeuwen, Jilles Vreeken, Koen Smets
 // All rights reserved.
-// 
+//
 // Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-// 
+//
 // Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
 // Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials // provided with the distribution.
 // Neither the name of the Universiteit Utrecht, Universiteit Antwerpen, nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-// 
+//
 #ifndef __BLOBAL_H
 #define __BLOBAL_H
 
@@ -48,6 +48,7 @@
 #if defined (__GNUC__)
 	#include <cstring>
 #endif
+#include <stdint.h>
 #include <stdio.h>
 #include <math.h>
 #include <map>
@@ -58,13 +59,22 @@
 
 using std::string;
 using std::vector;
+typedef uint32_t uint32;
+typedef uint16_t uint16;
+typedef uint8_t uint8;
+typedef uint64_t uint64;
 
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int32_t LONG;
+typedef int64_t int64;
 // Dit moet -na- STL shit staan
 // note-to-mvl: stl is niet cool
 #ifdef _MEMLEAK
 # ifdef _DEBUG
 #	define new DEBUG_NEW
-# endif 
+# endif
 #endif
 
 #if defined (__unix__)
